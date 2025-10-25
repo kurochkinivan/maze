cb08d353186bd4e12c73ccc518aeaa23ca6d5b95
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-10-25 20:40:27 +0700

    chore: remove output from domain level

---
+++ internal/domain/entities/path.go
@@ -0,0 +1,26 @@
+package entities
+
+type Path struct {
+	Cells []*Cell
+}
+
+func BuildPath(previous map[*Cell]*Cell, end *Cell) *Path {
+	path := []*Cell{}
+	current := end
+
+	for current != nil {
+		path = append(path, current)
+		current = previous[current]
+	}
+
+	return &Path{
+		Cells: path,
+	}
+}
+
+func (p *Path) ReversePath() *Path {
+	for i, j := 0, len(p.Cells)-1; i < j; i, j = i+1, j-1 {
+		p.Cells[i], p.Cells[j] = p.Cells[j], p.Cells[i]
+	}
+	return p
+}
